8138344e303e5efad2e792d7d0c2bb07d2512c9d
3dcAdmin <mantas.laikunas@gmail.com>
2025-01-05 20:27:08 +0200

    Update navigation.ts

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,10 +1,10 @@
 // An array of links for navigation bar
 const navBarLinks = [
-  { name: "Home", url: "/" },
-  { name: "Products", url: "/products" },
-  { name: "Services", url: "/services" },
-  { name: "Blog", url: "/blog" },
-  { name: "Contact", url: "/contact" },
+  { name: "Namai", url: "/" },
+  { name: "Paslaugos", url: "/products" },
+  { name: "Darbai", url: "/services" },
+  { name: "Apie mus", url: "/blog" },
+  { name: "Kontaktai", url: "/contact" },
 ];
 // An array of links for footer
 const footerLinks = [
